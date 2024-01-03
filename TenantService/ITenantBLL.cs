83e8babc1290f5809e3019355632f4a25c23aa48
AzfarSikandar <azfars@arkitektz.com>
2024-01-03 16:52:12 +0500

    remove unnecessary files

--- TenantService/ITenantBLL.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TenantSubscriptionApp.TenantService
-{
-    public interface ITenantBLL
-    {
-        Task<string> CreateConnectionString(string connectionString);
-
-        Task<string> GetTenantConnectionStringByUserId(string userId, string appName);
-    }
-}
